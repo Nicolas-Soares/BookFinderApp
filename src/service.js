cc97ba4a04e185261ff1a005f5a045342bd25dd2
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-06-03 04:29:10 -0300

    Update service.js
    Filtration of data from Google Books API;

--- src/service.js
+++ src/service.js
@@ -4,10 +4,43 @@ const URL = 'https://www.googleapis.com/books/v1/volumes?q='
 module.exports = {
     async getBooks(req, res) {
         const { search } = req.body
-        //console.log(search)
-        const url = `${URL}${search}&maxResults=1&key=AIzaSyDIND63Uyj-oFt1p1efn1EpVjNVMaP2lz0`
+        const url = `${URL}${search.replace(/ +/g, '')}&key=AIzaSyDIND63Uyj-oFt1p1efn1EpVjNVMaP2lz0`
         const response = await axios.get(url)
-        
-        console.log(response.data.items[0].volumeInfo.title)
+        const filtered = response.data.items.slice(0, 10)
+
+        function CreateItem() {
+            return {
+                title: null,
+                authors: null,
+                publishedDate: null,
+                imageLink: null,
+                previewLink: null
+            }
+        }
+
+        const itemList = []
+
+        let i = 0
+
+        for (i in filtered) {
+            let item = new CreateItem()
+            if (!filtered[i].volumeInfo.imageLinks) {
+                item.title = filtered[i].volumeInfo.title
+                item.authors = filtered[i].volumeInfo.authors
+                item.publishedDate = filtered[i].volumeInfo.publishedDate
+                item.imageLink = 'https://books.google.com.br/googlebooks/images/no_cover_thumb.gif'
+                item.previewLink = filtered[i].volumeInfo.previewLink
+            } else {
+                item.title = filtered[i].volumeInfo.title
+                item.authors = filtered[i].volumeInfo.authors
+                item.publishedDate = filtered[i].volumeInfo.publishedDate
+                item.imageLink = filtered[i].volumeInfo.imageLinks.thumbnail
+                item.previewLink = filtered[i].volumeInfo.previewLink
+            }
+
+            itemList.push(item)
+        }
+
+        return res.json(itemList)
     }
 }
