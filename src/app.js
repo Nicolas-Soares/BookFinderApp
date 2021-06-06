bd43327f71069fbd8c64f8b12feb8664132eaac4
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-06-06 15:17:29 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,4 +17,4 @@ app.use(bodyParser.urlencoded({
 app.use(routes)
 app.use(express.static(__dirname + '/public'))
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'))
